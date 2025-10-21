0ab68a84dda0111e92c4db596e7b21f289b8d1d9
murmeowcode <grisha23.01.2004@gmail.com>
2025-10-21 13:09:35 +0300

    update model

--- authentication_service/models/user.py
+++ authentication_service/models/user.py
@@ -1,31 +1,24 @@
-"""модель аутентифицированного пользователя"""#pylint: disable=E0401
+"""модель аутентифицированного пользователя"""
+# pylint: disable=E0401
 import uuid
-from sqlalchemy import Column, String, DateTime, Integer, BigInteger
+from sqlalchemy import Column, String, DateTime, Date
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import relationship
 from shared.database.database import Base
 
 class AuthUser(Base):
-    """модель"""
+    """модель аутентифицированного пользователя"""
     __tablename__ = "auth_users"
 
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     username = Column(String(50), unique=True, nullable=False, index=True)
     email = Column(String(100), unique=True, nullable=False, index=True)
-    role = Column(Integer,nullable = False)
+    surname = Column(String(50), nullable=False)
+    name = Column(String(50), nullable=False)
+    fathername = Column(String(50), nullable=True)  # Отчество может быть не у всех
+    birth_date = Column(Date, nullable=True)  # Правильно указано поле даты рождения
     hashed_password = Column(String(255), nullable=True)
     last_login = Column(DateTime, nullable=True)
-    password_reset_tokens = relationship("PasswordResetToken", back_populates="user")
-
-    vk_id = Column(BigInteger, unique=True, nullable=True, index=True)
-    oauth_provider = Column(String(20), nullable=True)
-    yandex_id = Column(String, unique=True, nullable=True, index=True)
 
     def __repr__(self):
         return f"<AuthUser {self.username}>"
-
-    @property
-    def is_oauth_user(self):
-        """property checking"""
-        return (self.vk_id is not None
-                or self.yandex_id is not None) and self.hashed_password is None
